872ebee3dcbc04fc11e9fdb13c7ab53bf17eec11
Charl Kruger <nit3watch@gmail.com>
2025-06-27 12:02:31 +0200

    d

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import type React from 'react';
+import React from 'react';
 import { useEffect, useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { HttpClient } from '@cloudflare/sandbox/client';

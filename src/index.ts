475bea29c022664b01f0dc898f7036e054a9a365
Charl Kruger <nit3watch@gmail.com>
2025-06-27 10:12:32 +0200

    h

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,12 @@ export { Sandbox } from '@cloudflare/sandbox';
 export default {
 	async fetch(request: Request, env: Env) {
 		const sandbox = getSandbox(env.Sandbox, 'my-sandbox');
-		return sandbox.exec('ls', ['-la']);
+		const result = await sandbox.exec('ls', ['-la']);
+
+		return new Response(JSON.stringify(result, null, 2), {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
 	},
 };

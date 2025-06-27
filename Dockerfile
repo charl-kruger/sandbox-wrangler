47d34b982870f934c1d9b783817cae4cc6244ddf
Charl Kruger <nit3watch@gmail.com>
2025-06-27 10:04:44 +0200

    d

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-# syntax=docker/dockerfile:1
-
+# If building your project on amd64, use:
 FROM docker.io/ghostwriternr/cloudflare-sandbox:0.0.5
+# If building your project on arm64, use:
+# FROM docker.io/ghostwriternr/cloudflare-sandbox-arm:0.0.5
 
 EXPOSE 3000
 
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,10 @@
-import { getSandbox, type Sandbox } from '@cloudflare/sandbox';
+import { getSandbox } from '@cloudflare/sandbox';
 
 export { Sandbox } from '@cloudflare/sandbox';
 
-type Env = {
-	Sandbox: DurableObjectNamespace<Sandbox>;
-};
-
 export default {
-	async fetch(request: Request, env: Env): Promise<Response> {
+	async fetch(request: Request, env: Env) {
 		const sandbox = getSandbox(env.Sandbox, 'my-sandbox');
-
-		await sandbox.gitCheckout('https://github.com/charl-kruger/basic-cloudflare-worker.git', {
-			branch: 'main',
-		});
-
-		const install = await sandbox.exec('npm', ['install'], {
-			stream: false,
-		});
-
-		console.log('npm i', install);
-
-		const result = await sandbox.exec('npx', ['wrangler', 'build'], {
-			stream: false,
-		});
-
-		console.log('result', result);
-
-		return new Response(JSON.stringify(result, null, 2), {
-			headers: {
-				'Content-Type': 'application/json',
-			},
-		});
+		return sandbox.exec('ls', ['-la']);
 	},
 };
